dc57e70569d85b634517ccf36c96dfcc962ce786
gaylesha <petr.vasilyev.1999@gmail.com>
2025-07-29 18:26:23 +0500

    Output of the wrapped error

---
+++ internal/lib/logger/sl/sl.go
@@ -0,0 +1,30 @@
+package sl
+
+import (
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envProd  = "prod"
+	env      = "debug"
+)
+
+func Err(err error) slog.Attr {
+	return slog.Attr{
+		Key:   "error",
+		Value: slog.StringValue(err.Error()),
+	}
+}
+
+func SetupSlog(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return log
+}
