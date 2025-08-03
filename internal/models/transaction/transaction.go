6a71ae9f976627874cd2555625f46669fa51bfbd
gaylesha <petr.vasilyev.1999@gmail.com>
2025-08-03 05:21:03 +0500

    del space

--- internal/models/transaction/transaction.go
+++ internal/models/transaction/transaction.go
@@ -3,9 +3,9 @@ package transaction
 import "time"
 
 type Request struct {
-	Id         int       `json: "id"`
+	Id         int       `json:"id"`
 	From       string    `json:"from"`
 	To         string    `json:"to"`
 	Amount     float64   `json:"amount"`
-	Created_at time.Time `json:"created_at`
+	Created_at time.Time `json:"created_at"`
 }
