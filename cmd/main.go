c88d37874cc4b6323b3862ff0b9665a9ed2ff995
gaylesha <petr.vasilyev.1999@gmail.com>
2025-08-03 05:08:32 +0500

    add init 10 wallet for first start

--- cmd/main.go
+++ cmd/main.go
@@ -7,42 +7,38 @@ import (
 	"github.com/Petro-vich/transaction_processing_go/internal/config"
 	httpserver "github.com/Petro-vich/transaction_processing_go/internal/http-server"
 	"github.com/Petro-vich/transaction_processing_go/internal/lib/logger/sl"
+	"github.com/Petro-vich/transaction_processing_go/internal/service/wallet"
 	"github.com/Petro-vich/transaction_processing_go/internal/storage/sqlite"
 )
 
 func main() {
 
-	config := config.Load()
-	log := sl.SetupSlog(config.Env)
+	cfg := config.Load()
+	log := sl.SetupSlog(cfg.Env)
 	log.Info("Start of the program")
 	log.Debug("debug messages are enabled")
 
-	storage, err := sqlite.New("/home/gaylesga/Desktop/s21.project/go/transaction_processing_go/storage/sqlite/storage.db")
-	//config.StoragePath
-	//TODO:
+	storage, err := sqlite.New(cfg.StoragePath)
+
 	if err != nil {
 		log.Error("failed to init storage", sl.Err(err))
 		os.Exit(1)
 	}
 
-	server := httpserver.New(storage, config, log)
-	log.Info("Starting server:", slog.String("address", config.Address))
-	if err := server.Start(); err != nil {
-		log.Error("failes to start server", sl.Err(err))
-		os.Exit(1)
+	if storage.IsEmpty() {
+		WallServ := wallet.NewService(storage)
+		if err := WallServ.InitWall(10); err != nil {
+			log.Error("failed to init pool wallets", sl.Err(err))
+		}
+		log.Info("the starter set of wallets has been added")
 	}
 
-	// walletInit := wallet.NewInitializer(storage)
-	// if err := walletInit.Initializer(10); err != nil { //TODO: yaml: `start_wallet_poop`
-	// 	log.Error("failed to init pool wallets", sl.Err(err))
-	// }
-	// log.Info("the starter set of wallets has been added")
+	server := httpserver.New(storage, cfg, log)
+	log.Info("Starting server:", slog.String("address", cfg.Address))
+	if err := server.Start(); err != nil {
+		log.Error("failed to start server", sl.Err(err))
 
-	// adr := "70200aebb271f81246dc200bbf8da3670f1568d541279cb32b822d454985777"
-	// balance, err := storage.GetBalance(adr)
-	// if err != nil {
-	// 	log.Error("failed to get balance", sl.Err(err))
-	// }
-	// fmt.Println(balance)
+		os.Exit(1)
+	}
 
 }
