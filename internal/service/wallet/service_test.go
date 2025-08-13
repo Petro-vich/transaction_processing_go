1fe3f6eaead020e99c1821d08b5d97e62e841e23
gaylesha <petr.vasilyev.1999@gmail.com>
2025-08-14 01:41:26 +0500

    add tests

--- internal/service/wallet/service_test.go
+++ internal/service/wallet/service_test.go
@@ -1,10 +1,10 @@
 package wallet
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/Petro-vich/transaction_processing_go/internal/models/transaction"
+	"github.com/Petro-vich/transaction_processing_go/internal/storage/sqlite"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
@@ -15,7 +15,7 @@ type mockStorage struct {
 
 func (_m *mockStorage) CreateWallet(address string, amount float64) error {
 	args := _m.Called(address, amount)
-	fmt.Println(args.Error(0))
+	//fmt.Println(args.Error(0))
 	return args.Error(0)
 }
 
@@ -35,15 +35,39 @@ func (_m *mockStorage) GetLast(count int) ([]transaction.Request, error) {
 }
 
 func TestWalletService_Initialize(t *testing.T) {
+	store := &mockStorage{}
+	service := NewService(store)
 	t.Run("Successful init", func(t *testing.T) {
-		store := &mockStorage{}
-		service := NewService(store)
 
-		store.On("CreateWallet", mock.AnythingOfType("string"), 100.0).Return(nil).Times(2)
+		store.On("CreateWallet", mock.AnythingOfType("string"), 100.0).Return(nil).Times(1)
 
-		err := service.InitWall(2)
-
+		err := service.InitWall(1)
 		assert.NoError(t, err)
 	})
 
+	t.Run("Negative count", func(t *testing.T) {
+
+		err := service.InitWall(-1)
+		assert.EqualError(t, err, "count can not be zero or negative")
+	})
+
+	t.Run("zero count", func(t *testing.T) {
+
+		err := service.InitWall(-1)
+		assert.EqualError(t, err, "count can not be zero or negative")
+	})
+}
+
+func BenchmarkInitWallSequential(b *testing.B) {
+	store, err := sqlite.New("file::memory:?cache=shared")
+	if err != nil {
+		b.Fatalf("failed to create test DB: %v", err)
+	}
+
+	service := NewService(store)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = service.InitWall(1000) // Тестируем с 1000 кошельков
+	}
 }
