5c5879800107b02d75f6610089eb6e3319718b49
gaylesha <petr.vasilyev.1999@gmail.com>
2025-08-14 01:41:07 +0500

    add sync

--- internal/service/wallet/service.go
+++ internal/service/wallet/service.go
@@ -3,6 +3,8 @@ package wallet
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"fmt"
+	"sync"
 
 	"github.com/Petro-vich/transaction_processing_go/internal/storage"
 )
@@ -17,15 +19,43 @@ func NewService(storage storage.Repository) *WalletService {
 }
 
 func (ws *WalletService) InitWall(count int) error {
+	if count <= 0 {
+		return fmt.Errorf("count can not be zero or negative")
+	}
+
+	var wg sync.WaitGroup
+	var chErr = make(chan error, count)
+	var mu sync.Mutex
+
 	for i := 0; count > i; i++ {
-		wallAdr, err := generateWalletAddress()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			wallAdr, err := generateWalletAddress()
+			if err != nil {
+				chErr <- err
+				return
+			}
+			mu.Lock()
+			err = ws.storage.CreateWallet(wallAdr, 100.0)
+			mu.Unlock()
+			if err != nil {
+				chErr <- err
+				return
+			}
+
+		}()
+	}
+
+	wg.Wait()
+
+	close(chErr)
+	for err := range chErr {
 		if err != nil {
 			return err
 		}
-		if err := ws.storage.CreateWallet(wallAdr, 100.0); err != nil {
-			return err
-		}
 	}
+
 	return nil
 }
 
